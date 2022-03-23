9ec1d549d91712d498817dbd947ed3cd3dea37a2
Joy Christine <joychristin2@gmail.com>
2022-03-24 02:02:15 +0300

    add filter to list of houses

---
+++ learn/scripts.js
@@ -0,0 +1,17 @@
+function myFunc() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("my-list");
+    li = document.getElementsByTagName("li");
+    for(i = 0; i < li.length;i++){
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContext || a.innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1){
+            li[i].display = "";
+        }else{
+            li[i].style.display = "none";
+        }
+
+    }
+}
