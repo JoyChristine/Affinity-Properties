ab523ba5b2c91aadb7021f9abeb12a1c97597fb8
Joy Christine <joychristin2@gmail.com>
2022-03-24 03:00:23 +0300

    add filter function

--- learn/scripts.js
+++ learn/scripts.js
@@ -1,4 +1,4 @@
-function myFunc() {
+function house() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -13,5 +13,28 @@ function myFunc() {
             li[i].style.display = "none";
         }
 
+    }
+}
+// CLEAR SEARCH BOX
+function clearInputFields(divElement) {
+    var ele = document.getElementById(divElement);
+
+    // IT WILL READ ALL THE ELEMENTS. <p>, <div>, <input> ETC.
+    for (i = 0; i < ele.childNodes.length; i++) {
+
+        // SINCE THE <input> FIELDS ARE INSIDE A <p> TAG, 
+        // I'LL USE THE "firstChild" PROPERTY TO GET THE <input> TAG.
+        var child = ele.childNodes[i].firstChild;
+        //console.log(child);
+
+        // CHECK IF CHILD NOT NULL.
+        // THIS IS IMPORTANT AS IT WILL RETURN A TEXT FOR EVERY "Whitespace".
+        // 'Whitespace' IS A TEXT OR NODE BETWEEN <div> AND <p> AND AFTER <p>.
+        if (child) {
+            switch (child.type) {
+                case 'text':
+                    child.value = '';
+            }
+        }
     }
 }
