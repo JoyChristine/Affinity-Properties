8b19bd0c02b80b361ae9b4d15a81b0ba6567f26a
Joy Christine <joychristin2@gmail.com>
2022-03-24 03:55:28 +0300

    add mouseover function over financial section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // JOY'S JS
+$(document).ready(function() {
+
+    $('.bank b').mouseover(
+       
+       function () {
+          $(this).css({"text-decoration":"underline"});
+    });
+    $(".bank b").mouseout(
+        function () {
+            $(this).css({"text-decoration":"none"});
+      });
+       
+ })
